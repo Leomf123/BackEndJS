33f9e0f13236af4533e796a5a1152e68582311b9
Leonardo Macedo Freire <a12455@ifmg.edu.br>
2025-10-22 21:53:46 -0300

    chore: Começo da atividade calculadora

---
+++ Aula22-10/exercCalculadora/script.js
@@ -0,0 +1,13 @@
+function calculadora(){
+    const num1 = Number(prompt("Forneça o primeiro número:"));
+    const num2 = Number(prompt("Forneça o segundo número"));
+
+    menu = 'Forneça o número da opção desejada:\n'+
+        '[1] Somar\n'+
+        '[2] Subtrair\n'+
+        '[3] Multiplicar\n'+
+        '[4] Dividir\n';
+    const operacao = Number(prompt());
+
+    
+}
