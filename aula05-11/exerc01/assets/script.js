aec2e84153a5996831c35e099f6eb4bb28be13ca
Leonardo Macedo Freire <a12455@ifmg.edu.br>
2025-11-05 21:50:24 -0300

    feat: exercicios

---
+++ aula05-11/exerc01/assets/script.js
@@ -0,0 +1,9 @@
+function verificaEstacao(mes){
+    const estacao = { 
+        'verao': ['dezembro', 'janeiro', 'fevereiro'], 
+        'outono': ['março', 'abril', 'maio'], 
+        'inverno': ['junho', 'julho', 'agosto'], 
+        'primavera': ['setembro', 'outubro', 'novembro']
+    };
+    console.log(estacao);
+}
