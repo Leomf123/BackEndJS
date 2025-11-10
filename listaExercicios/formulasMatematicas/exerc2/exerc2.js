3605ed319239ab819146786c17045c1d4f8b7a1a
Leonardo Macedo <lmacedo.freire@gmail.com>
2025-11-10 20:48:22 -0300

    feat: Adiciona exerc. 2 lista 1

---
+++ listaExercicios/formulasMatematicas/exerc2/exerc2.js
@@ -0,0 +1,11 @@
+/*
+    2. Escreva um programa que calcule a média aritmética de dois números
+*/
+
+function mediaAritmetica(numero1, numero2){
+    const media = (numero1 + numero2)/ 2;
+    
+    return media;
+}
+
+console.log(mediaAritmetica(10, 90));
