34f0a7291130c06e9d4e665473d00f8d2b2286d9
Leonardo Macedo <lmacedo.freire@gmail.com>
2025-11-10 20:43:33 -0300

    feat: Adiciona exerc. 1 lista de exercicios

---
+++ listaExercicios/formulasMatematicas/exerc1/assets/js/exerc1.js
@@ -0,0 +1,18 @@
+function operacoes(){
+    const numero1 = Number(prompt("Forneça o primeiro número:"));
+    const numero2 = Number(prompt("Forneça o segundo número:"));
+
+    const soma = numero1 + numero2;
+    const subtracao = numero1 - numero2;
+    const multiplicacao = numero1 * numero2;
+    const divisao = numero1 / numero2;
+
+    alert(`Os resultados das operações entre os numeros ${numero1} e ${numero2} são:
+                Soma: ${soma}
+                Subtração: ${subtracao}
+                Multiplicação: ${multiplicacao}
+                Divisão: ${divisao}
+        `)
+}
+
+operacoes();
