6f7b38af81fc2ae0a023a26903eb41d788a12844
Leonardo Macedo Freire <a12455@ifmg.edu.br>
2025-11-12 21:31:47 -0300

    feat: Adiciona anotações aula dia 12/11/25

---
+++ aula17/aula17.js
@@ -0,0 +1,28 @@
+
+
+// try
+
+// catch
+
+// throw
+
+// finally
+
+try{
+    console.log(a);
+}catch(e){
+    console.log(e.message);
+}finally{
+    console.log('Continua...');''
+}
+
+
+console.log('Continua...');
+
+console.log('Continua...');
+
+
+//setTimeout(funcao, tempo em milisegundos)
+
+// setInterval(funcao, tempo em milisegundos)
+//clearInterval
